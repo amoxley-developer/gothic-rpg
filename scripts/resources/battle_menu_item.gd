91b4854fbdc742d1a58befd5df98eebdd55a84cc
amoxley <alex.moxley.dev@gmail.com>
2025-11-16 19:47:46 -0800

    add menu navigation logic

--- scripts/resources/battle_menu_item.gd
+++ scripts/resources/battle_menu_item.gd
@@ -3,6 +3,3 @@ extends Resource
 
 @export var name: String
 @export var sub_menu_items: Array[SubMenuItem]
-
-func on_select() -> void:
-    print(sub_menu_items)
--- scripts/scenes/player_ui.gd
+++ scripts/scenes/player_ui.gd
@@ -5,23 +5,45 @@ enum States {UNSELECTED, SUBMENU_OPENED, SELECTED}
 
 @export var menu_items: Array[BattleMenuItem]
 
+var current_menu_item: BattleMenuItem
+var current_menu_item_idx := 0
+
+
 var state: States = States.UNSELECTED
 
+func _ready() -> void:
+	current_menu_item = menu_items[current_menu_item_idx]
+	print(current_menu_item.name)
+
 func _process(_delta: float) -> void:
-    if Input.is_action_just_pressed("accept"):
-        if state != States.SELECTED:
-            set_state(state+1)
-        else:
-            # print the player selection's option
-            set_state(States.UNSELECTED)
-    if Input.is_action_just_pressed("cancel"):
-        if state != States.UNSELECTED:
-            set_state(state-1)
+	if Input.is_action_just_pressed("accept"):
+		if state != States.SELECTED:
+			set_state(state+1)
+	if Input.is_action_just_pressed("cancel"):
+		if state != States.UNSELECTED:
+			set_state(state-1)
+	# Unselected state
+	if state == States.UNSELECTED:
+		if Input.is_action_just_pressed('ui_up') or Input.is_action_just_pressed('ui_down'):
+			handle_unselected_menu_navigation()
+
+			
+func handle_unselected_menu_navigation() -> void:
+	var menu_items_size = menu_items.size()
+	if Input.is_action_just_pressed('ui_up'):
+			if current_menu_item_idx == 0:
+				current_menu_item_idx = menu_items_size - 1
+			else:
+				current_menu_item_idx -= 1
+	elif Input.is_action_just_pressed('ui_down'):
+		current_menu_item_idx += 1
+		if current_menu_item_idx == menu_items_size:
+			current_menu_item_idx = 0
+	current_menu_item = menu_items[current_menu_item_idx]
+	print('current menu item: ', current_menu_item.name)
 
 func set_state(new_state: States) -> void:
-    var previous_state := state
-    state = new_state
-    print('previous_state: ', previous_state)
-    print('current_state: ', state)
-
-
+	var previous_state := state
+	state = new_state
+	print('previous_state: ', States.keys()[previous_state])
+	print('current_state: ', States.keys()[state])
