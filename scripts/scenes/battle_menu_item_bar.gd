e64158ad7d0ad90d1b803ae78d2cc869fd05f373
amoxley <alex.moxley.dev@gmail.com>
2025-11-23 20:15:09 -0800

    create the menu item bar

--- scripts/scenes/battle_menu_item_bar.gd
+++ scripts/scenes/battle_menu_item_bar.gd
@@ -2,10 +2,25 @@ class_name BattleMenuItemBar
 extends Node2D
 
 @export var menu_resource: BattleMenuItem
-@export var menu_name: String
-@export var subMenuItems: Array[SubMenuItem]
+@export var menu_name_node: RichTextLabel
+var menu_name: String
+var sub_menu_items: Array[SubMenuItem]
+var is_active := false
 
 func _ready() -> void:
-  # set the name and subMenuItems
+  menu_name = menu_resource.menu_name
+  sub_menu_items = menu_resource.sub_menu_items
+  set_menu_label()
+
+func set_menu_label() -> void:
+  menu_name_node.text = menu_name
+
+func set_menu_active() -> void:
+  # change color to white
+  is_active = true
   return
-# set a sprite object in godot
+
+func set_menu_inactive() -> void:
+  # change color to grey
+  is_active = false
+  return
--- scripts/scenes/player_ui.gd
+++ scripts/scenes/player_ui.gd
@@ -2,17 +2,20 @@ class_name PlayerUI
 extends Node2D
 
 enum States {UNSELECTED, SUBMENU_OPENED, SELECTED}
+const MENU_HEIGHT := 32
 
-@export var menu_items: Array[BattleMenuItem]
-
-var current_menu_item: BattleMenuItem
+const battleMenuItemBarScn := preload("res://scenes/battle_menu_item_bar.tscn")
+@export var menu_items_resources: Array[BattleMenuItem]
+var menu_items: Array[BattleMenuItemBar]
+var current_menu_item: BattleMenuItemBar
 var current_menu_item_idx := 0
 
 
 var state: States = States.UNSELECTED
 
 func _ready() -> void:
-	current_menu_item = menu_items[current_menu_item_idx]
+	create_menu_items()
+	set_current_menu_item(current_menu_item_idx)
 	print(current_menu_item.menu_name)
 
 func _process(_delta: float) -> void:
@@ -40,11 +43,29 @@ func handle_menu_navigation() -> void:
 			current_menu_item_idx += 1
 			if current_menu_item_idx == menu_items_size:
 				current_menu_item_idx = 0
-		current_menu_item = menu_items[current_menu_item_idx]
+		set_current_menu_item(current_menu_item_idx)
+		
 		print('current menu item: ', current_menu_item.menu_name)
+
+func set_current_menu_item(idx: int) -> void:
+	if (current_menu_item != null):
+		current_menu_item.set_menu_inactive()
+
+	current_menu_item = menu_items[idx]
+	current_menu_item.set_menu_active()
 
 func set_state(new_state: States) -> void:
 	var previous_state := state
 	state = new_state
 	print('previous_state: ', States.keys()[previous_state])
 	print('current_state: ', States.keys()[state])
+
+func create_menu_items() -> void:
+	var y_pos := 0
+	for menu_resource in menu_items_resources:
+		var battle_menu_item_bar: BattleMenuItemBar = battleMenuItemBarScn.instantiate()
+		battle_menu_item_bar.menu_resource = menu_resource
+		battle_menu_item_bar.position = Vector2(0, y_pos)
+		menu_items.append(battle_menu_item_bar)
+		add_child(battle_menu_item_bar)
+		y_pos += MENU_HEIGHT + 8
