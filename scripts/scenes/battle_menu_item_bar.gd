40b3aee99a7ce15ce249498689727690d3de533f
amoxley <alex.moxley.dev@gmail.com>
2025-12-01 21:26:32 -0800

    create the rendering of the sub menu items

--- scripts/scenes/battle_menu_item_bar.gd
+++ scripts/scenes/battle_menu_item_bar.gd
@@ -3,14 +3,17 @@ extends Node2D
 
 @export var menu_resource: BattleMenuItem
 @export var menu_name_node: RichTextLabel
+
+var sub_menu_resource: SubMenuItem
 var menu_name: String
-var sub_menu_items: Array[SubMenuItem]
+var sub_menu_items: Array[SubMenuItem] = []
 
 func _ready() -> void:
-	
-	menu_name = menu_resource.menu_name
-	sub_menu_items = menu_resource.sub_menu_items
-	menu_name_node.bbcode_enabled = true
+	if menu_resource != null: 
+		menu_name = menu_resource.menu_name
+		sub_menu_items = menu_resource.sub_menu_items
+	if sub_menu_resource != null:
+		menu_name = sub_menu_resource.name
 	set_menu_label(Enums.MenuItemState.INACTIVE)
 
 func set_menu_label(menu_item_state: Enums.MenuItemState) -> void:
--- scripts/scenes/player_ui.gd
+++ scripts/scenes/player_ui.gd
@@ -2,13 +2,16 @@ class_name PlayerUI
 extends Node2D
 
 enum States {UNSELECTED, SUBMENU_OPENED, SELECTED}
+
 const MENU_HEIGHT := 16
+const MENU_WIDTH := 64
 
 @onready var placeholderLabel: RichTextLabel = $RichTextLabel
 
 const battleMenuItemBarScn := preload("res://scenes/battle_menu_item_bar.tscn")
 @export var menu_items_resources: Array[BattleMenuItem]
 var menu_items: Array[BattleMenuItemBar]
+var sub_menu_items: Array[BattleMenuItemBar]
 var current_menu_item: BattleMenuItemBar
 var current_menu_item_idx := 0
 
@@ -30,11 +33,13 @@ func handle_menu_selection() -> void:
 		match state:
 			States.UNSELECTED:
 				current_menu_item.set_menu_label(Enums.MenuItemState.ACTIVE)
+				create_sub_menu_items()
 		set_state(state+1)
 	if Input.is_action_just_pressed("cancel") and state != States.UNSELECTED:
 		match state:
 			States.SUBMENU_OPENED:
 				current_menu_item.set_menu_label(Enums.MenuItemState.HIGHLIGHTED)
+				delete_sub_menu_items()
 		set_state(state-1)
 			
 func handle_menu_navigation() -> void:
@@ -69,7 +74,23 @@ func create_menu_items() -> void:
 	for menu_resource in menu_items_resources:
 		var battle_menu_item_bar: BattleMenuItemBar = battleMenuItemBarScn.instantiate()
 		battle_menu_item_bar.menu_resource = menu_resource
+		# create a shared function here
 		battle_menu_item_bar.position = Vector2(0, y_pos)
 		menu_items.append(battle_menu_item_bar)
 		add_child(battle_menu_item_bar)
 		y_pos += MENU_HEIGHT + 8
+
+func create_sub_menu_items() -> void:
+	var y_pos := 0
+	for submenu_resource in current_menu_item.sub_menu_items:
+		var sub_battle_menu_item_bar: BattleMenuItemBar = battleMenuItemBarScn.instantiate()
+		sub_battle_menu_item_bar.sub_menu_resource = submenu_resource
+		sub_battle_menu_item_bar.position = Vector2(MENU_WIDTH + 16, y_pos)
+		sub_menu_items.append(sub_battle_menu_item_bar)
+		add_child(sub_battle_menu_item_bar)
+		y_pos += MENU_HEIGHT + 8
+
+func delete_sub_menu_items() -> void:
+	for sub_menu_item in sub_menu_items:
+		sub_menu_item.queue_free()
+	sub_menu_items = []
