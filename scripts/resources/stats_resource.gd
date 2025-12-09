c53ee470130d5f5729ec8b7a3327f859a6b9a98a
amoxley <alex.moxley.dev@gmail.com>
2025-12-08 21:08:02 -0800

    create the stats resource

---
+++ scripts/resources/stats_resource.gd
@@ -0,0 +1,9 @@
+class_name StatsResource
+extends Resource
+
+@export var health: int
+@export var mana: int
+@export var attack: int
+@export var defense: int
+@export var speed: int
+@export var luck: int
