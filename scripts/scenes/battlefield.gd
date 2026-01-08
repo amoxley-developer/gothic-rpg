ba90ef11d335bf62e2ed656c15a05c5217b831c9
amoxley <alex.moxley.dev@gmail.com>
2026-01-07 19:18:06 -0800

    add tween movement for the player

--- scripts/scenes/battlefield.gd
+++ scripts/scenes/battlefield.gd
@@ -10,6 +10,8 @@ extends Node2D
 @onready var attack_post_timer: Timer = $AttackTimers/PostTimer
 
 var current_attack: String
+var tween: Tween
+var attacked: bool
 
 enum BattlefieldState {UI_SELECTION, PLAYER_ATTACK, ENEMY_ATTACK}
 
@@ -22,12 +24,13 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("accept"):
 		if current_state == BattlefieldState.PLAYER_ATTACK:
-			execute_attack()
+			if current_attack == 'Crossbow':
+				execute_crossbow_attack()
 
 func _on_player_attack(attack_name: String) -> void:
 	set_state(BattlefieldState.PLAYER_ATTACK)
 	if attack_name == 'Sword':
-		enemy.take_damage(3)
+		start_sword_attack()
 	elif attack_name == 'Crossbow':
 		start_crossbow_attack()
 	current_attack = attack_name
@@ -40,46 +43,58 @@ func set_state(new_state: BattlefieldState) -> void:
 		playerUI.pause_ui()
 	current_state = new_state
 
+func start_sword_attack() -> void:
+	const pre_timer_length := .5
+	attack_pre_timer.wait_time = pre_timer_length
+	attack_initial_timer.wait_time = 1.5
+	attack_optimal_timer.wait_time = 1
+
+	if tween:
+		tween.kill()
+	tween = create_tween()
+	var new_x := enemy.position.x - 64
+	tween.tween_property(player, "position", Vector2(new_x, player.position.y), pre_timer_length).set_trans(Tween.TRANS_SINE);
+	attack_pre_timer.start()
+
 func start_crossbow_attack() -> void:
 	attack_pre_timer.wait_time = .5
 	attack_initial_timer.wait_time = 1
 	attack_optimal_timer.wait_time = 1
 	attack_pre_timer.start()
 
-func execute_attack() -> void:
-	if not attack_pre_timer.is_stopped():
+func execute_crossbow_attack() -> void:
+	if not attack_pre_timer.is_stopped() or attacked:
 		return
 	var attack_multiplier := 1.0
 	if not attack_initial_timer.is_stopped():
 		attack_multiplier = 0.5
 	elif not attack_optimal_timer.is_stopped():
 		attack_multiplier = 2.0
-	var base_damage := 0
-	if current_attack == 'Crossbow':
-		base_damage = 2
-	else:
-		base_damage = 3
 
 	attack_pre_timer.stop()
 	attack_initial_timer.stop()
 	attack_optimal_timer.stop()
 
-	enemy.take_damage(ceil(base_damage * attack_multiplier))
+	enemy.take_damage(ceil(2 * attack_multiplier))
+	attacked = true
 	attack_post_timer.start()
 
 func _on_pre_timer_timeout() -> void:
-	attack_initial_timer.start()
-	player.display_attack_sprite()
+	if current_attack == 'Crossbow':
+		attack_initial_timer.start()
+		player.display_attack_sprite()
 
 func _on_initial_timer_timeout() -> void:
 	attack_optimal_timer.start()
 	player.display_optimal_attack_sprite()
 
 func _on_optimal_timer_timeout() -> void:
-	execute_attack()
+	if (current_attack == 'Crossbow'):
+		execute_crossbow_attack()
 
 func _on_post_timer_timeout() -> void:
 	playerUI.reset_menu()
 	player.display_default_sprite()
 	playerUI.unpause_ui()
+	attacked = false
 	set_state(BattlefieldState.UI_SELECTION)
